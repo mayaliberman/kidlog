3ab11fba1fa5370b89d5a86441f91f31f62a2ae4
unknown <mayaliberman1@gmail.com>
2020-04-21 11:59:01 +0300

    elad lesson 210420

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 require("dotenv").config();
-
+const morganBody = require('morgan-body');
 const express = require("express");
 const app = express();
 
+
+const postsRouter = require("./routes/posts");
+const usersRouter = require("./routes/users");
+const childrenRouter = require('./routes/children');
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+morganBody(app);
+
+app.use("/posts", postsRouter);
+app.use("/users", usersRouter);
+app.use('/children', childrenRouter);
 
 const mongoose = require("mongoose");
 mongoose.set("useUnifiedTopology", true);
 
-mongoose.connect(
-  process.env.DATABASE_URL,
-  { useNewUrlParser: true }
-  
-);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to database"));
- 
 
-const postsRouter = require('./routes/posts');
-const usersRouter = require("./routes/users");
-app.use('/posts', postsRouter);
-app.use('/users',usersRouter);
+
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.status(404).send('page not found')
-})
-app.listen(3003, () => console.log("server started"));
+  res.status(404).send("page not found");
+});
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true },
+
+  () => {
+    app.listen(3003, () => console.log("server started"));
+  }
+);
