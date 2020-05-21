2b2aad7aa08e89115244c75433a0cf2384d0569c
unknown <mayaliberman1@gmail.com>
2020-05-21 15:36:02 +0300

    xss and mongo sanitization

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morganBody = require('morgan-body');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController.js');
 const postsRouter = require('./routes/posts');
@@ -31,6 +32,12 @@ app.use('/', limiter);
 
 //Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10k' }));
+
+//Data sanitization again NoSQL query injections
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
 
 //CORS support cross-origin resource sharing or CORS
 // app.use(cors());
