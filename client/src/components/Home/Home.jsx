10576aa31e1d1f969e670b3da8d4911fdf92d766
unknown <mayaliberman1@gmail.com>
2020-07-05 10:22:01 +0300

    updated home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { content, title, button } from './Home.module.scss';
+import { content, title, button, signIn } from './Home.module.scss';
 import logo from '../../assets/Logo_white_splash.svg';
 import { Link } from 'react-router-dom';
 export const Home = () => {
@@ -8,16 +8,14 @@ export const Home = () => {
       <img alt='company logo' src={logo} />
       <h1 className={title}>Every kid can become multi-talent</h1>
       <h3>Track your kid's progress and share your experiences</h3>
-      <Link to='/sign-in' className={button}>
-        Sign In
-      </Link>
-      <Link
-        to='/sign-up'
-        className={button}
-        style={{ backgroundColor: '#6f8ba4' }}
-      >
+      <Link to='/sign-up' className={button}>
         Sign Up
       </Link>
+      <div className={signIn}>
+        <p>
+          Already have an account? <Link to='/sign-in'>Sign in</Link>
+        </p>
+      </div>
     </div>
   );
 };
