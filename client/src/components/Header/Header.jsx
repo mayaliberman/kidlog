56b01ffc8621e53e1d7a647e1d0eec11e94ce335
unknown <mayaliberman1@gmail.com>
2020-07-26 16:10:03 +0300

    header conditional by getUser updated

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,40 +8,27 @@ import {
   logoutIcon,
   navHeader,
 } from './Header.module.scss';
-import { useLocation } from 'react-router-dom';
 import avatarIcon from '../../assets/image-4.svg';
 import logo from '../../assets/Logo_white_splash.svg';
 import LogoutIcon from '../../assets/logout.svg';
 import AuthContext from '../../context/auth/authContext';
-import UserContext from '../../context/user/userContext';
 import { getUser } from '../../services/cookies';
 import { Link } from 'react-router-dom';
 const Header = () => {
-  const userContext = useContext(UserContext);
-  const { isUpdated } = userContext;
   const authContext = useContext(AuthContext);
   const { logout } = authContext;
   let header = null;
-  const location = useLocation();
+
   let user = getUser();
   useEffect(() => {
     user = getUser();
-  }, [isUpdated]);
-  if (
-    // isLogged
-    location.pathname === '/' ||
-    location.pathname === '/sign-in' ||
-    location.pathname === '/sign-up' ||
-    location.pathname === '/add-kid' ||
-    location.pathname === '/forgot-password'
-  )
-    header = <></>;
+  }, [user]);
+  if (!user) header = <></>;
   else
     header = (
       <div className={navHeader}>
         <div className={nav}>
           <img alt='company logo' src={logo} className={logoIcon} />
-          {/* <h3 className={headerTitle}>{location.pathname.slice(1)}</h3> */}
           <div className={account}>
             <Link to='/my-account' className={account}>
               <img alt='avatar' src={avatarIcon} className={avatar} />
